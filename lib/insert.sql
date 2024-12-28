c0aa531d7b3398a2f7c8e61a3a84b1f0487639d6
Dave Banwell <dave@davebanwell.com>
2024-12-28 17:25:14 -0500

    all tests passing

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+("Mr, Chocolate", 4, "M", "brown", "gentle", 1),
+("Rowdy", 7,"M", "brown", "destructive", 1),
+("Tabitha", 3, "F", "black", "curious", 0),
+("Sergeant Brown", 1,"M", "white", "docile", 1),
+("Melissa", 5, "F", "brown", "anxious", 1),
+("Grinch", 9,"M", "black", "miserly", 0),
+("Wendy", 6, "F", "brown", "avoidant", 1),
+(NULL, 3,"M", "black", "friendly", 1);
